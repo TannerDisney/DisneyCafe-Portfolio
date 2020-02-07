f9d7b084b6914957365361b06c3abc54880672d4
Tanner Disney <disneytanner@gmail.com>
2020-02-07 13:33:35 -0800

    Added a post method to allow the ContactUs Form to send the message

--- DisneyCafe/Controllers/HomeController.cs
+++ DisneyCafe/Controllers/HomeController.cs
@@ -31,6 +31,20 @@ namespace DisneyCafe.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Contact(ContactForm contact)
+        {
+            if (ModelState.IsValid)
+            {
+                string autoMsg = $"<strong>This message was sent via disneycafe.online and from the email address {contact.EmailAddress}</strong><br>" + contact.Body;
+                await SendGridEmailHelper.SendEmailToUser(_config, "INSERT COPERATE EMAIL", contact.Subject, autoMsg);
+                TempData["WasSuccessful"] = "Your email was sent to DisneyCafe successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
