21d35b1264d91f35aa2920170ac19c20d7c59847
Tanner Disney <disneytanner@gmail.com>
2020-02-07 12:38:12 -0800

    Using SendGrid's Api the website is now able to send emails to user.

---
+++ DisneyCafe/Models/SendGridEmailHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace DisneyCafe.Models
+{
+    public static class SendGridEmailHelper
+    {
+        public static async Task SendEmailToUser(IConfiguration config, string emailAddress, string subject = "Regarding DisneyCafe", string body = "Automated Message From SendGridApi")
+        {
+            var apiKey = config.GetSection("SendGridApiKey").Value;
+            var client = new SendGridClient(apiKey);
+            var doNotReplyEmail = new EmailAddress("change@gmail.com");
+            EmailAddress recipient = new EmailAddress(emailAddress);
+            var htmlContent = $"<strong>DisneyCafe Inc.</strong><br><p>{body}</p>";
+            var msg = MailHelper.CreateSingleEmail(doNotReplyEmail, recipient, subject, body, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
